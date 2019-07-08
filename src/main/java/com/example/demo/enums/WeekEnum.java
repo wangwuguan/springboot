e8f361fd23cdac78efaf7e809537dea506b75223
wangpengxiang <2047683649@qq.com>
2019-07-08 19:18:09 +0800

    提交

--- src/main/java/com/example/demo/enums/WeekEnum.java
+++ src/main/java/com/example/demo/enums/WeekEnum.java
@@ -7,19 +7,17 @@ import lombok.ToString;
 @Getter
 public enum WeekEnum {
 
-    /* 星期一 */
+    /** 星期一 */
     MONDAY( 1, "星期一"),
 
-    /* 星期二 */
+    /** 星期二 */
     THUSDAY(2, "星期二"),
 
-    /* 星期三 */
+    /** 星期三 */
     WEDNESDAY(3, "星期三");
 
     private int dayId;
-
     private String dayName;
-
 
     WeekEnum(int dayId, String dayName) {
         this.dayId = dayId;
